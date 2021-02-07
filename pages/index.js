8c160f1bb9c5095853142774cec0815819696d66
Whatdapep V <pomming1997@gmail.com>
2021-02-07 22:03:56 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ posts }) {
+  const { articles } = posts;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,45 +11,47 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="text-center mx-auto">
+        <h1>เนื้อหาข่าวสาร API FREE</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {posts &&
+          articles &&
+          articles &&
+          articles.map((val,i) => {
+            // console.log(val)
+            return (
+              <div className="py-2" key={i}>
+                <a href={val.url} target="_blank" rel="no">
+                  <h2 className="font-medium text-gray-800 text-lg">
+                    {val.title}
+                  </h2>
+                </a>
+                <div className="flex ">
+                  <div className="w-1/3 mx-auto flex justify-center">
+                    <img
+                      src={`${val.urlToImage}`}
+                      className="w-60 h-42 rounded-sm"
+                      alt={`${val.title}`}
+                    />
+                  </div>
+                  <div className="w-2/3">
+                    <h3 className="font-light text-gray-600 text-left">
+                      {val.description}
+                    </h3>
+                    <br />
+                    <h4 className="font-light text-gray-600 text-sm text-left">
+                      {val.author} {val.publishedAt}
+                    </h4>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+                    <h4 className="text-sm text-left">
+                      อ้างอิง
+                      {val.source.name}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +60,23 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=th&apiKey=f4693406f98e42158b4d6022c6fc04ae"
+  );
+  const posts = await res.json();
+  // console.log(posts);
+  return {
+    props: {
+      posts,
+    },
+  };
 }
